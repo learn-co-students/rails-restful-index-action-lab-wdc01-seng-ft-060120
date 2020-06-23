c20e219b3a18edb4f739d99fbd0e493d0518119d
DannyCoop <cooperdaniel5141@gmail.com>
2020-06-23 12:51:45 -0400

    made the view file connect to the controller

--- app/controllers/student_controller.rb
+++ app/controllers/student_controller.rb
@@ -3,7 +3,7 @@ class StudentController < ApplicationController
     
     def index
         @students = Student.all
-       render :student
+       render :index
     end
     
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'student', to: 'student#index', as: 'student'
+  get '/students', to: 'student#index', as: 'student'
 end
